d1aa8bcdb542f0eac08687b112f8b681ff4fa848
Dmytro Pashko <pashko.dmytro@gmail.com>
2020-04-20 23:46:06 +0300

    Fixed issue with alpha blending...

--- core/src/com/dpashko/sandbox/scene/model3d/Model3dController.kt
+++ core/src/com/dpashko/sandbox/scene/model3d/Model3dController.kt
@@ -1,6 +1,8 @@
 package com.dpashko.sandbox.scene.model3d
 
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.graphics.g3d.attributes.BlendingAttribute
 import com.badlogic.gdx.math.Vector3
 import com.dpashko.sandbox.model.ModelsProvider
 import com.dpashko.sandbox.objects.WorldObject
@@ -15,6 +17,9 @@ class Model3dController @Inject constructor() : Controller {
 
     override fun init() {
         ground = ModelsProvider.loadGround().apply {
+
+            materials[1].set(BlendingAttribute(true, GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA, 1.0f))
+
             transform.set(Vector3.X, Vector3.Y, Vector3.Z, Vector3.Zero)
             objects.add(WorldObject(this))
         }
