c56edf198b79a5ceb0046ad8a94323ac42302895
Dmytro Pashko <pashko.dmytro@gmail.com>
2020-06-22 02:43:31 +0300

    Added fps counter.

--- core/src/com/dpashko/sandbox/scene/editor/EditorScene.kt
+++ core/src/com/dpashko/sandbox/scene/editor/EditorScene.kt
@@ -15,6 +15,8 @@ class EditorScene(skin: Skin) : Scene, ChangeListener() {
     private var table = Table(skin)
     private var worldSizeSelectBox = SelectBox<WorldSize>(skin)
     private var worldSizeLabel = Label("World size: ", skin)
+    private var fpsLabel = Label("FPS : ", skin)
+    private var fpsValue = Label("", skin)
     private var drawGridCheckBox = CheckBox("Draw grid", skin)
     private var drawAxisCheckBox = CheckBox("Draw axis", skin)
     private val controller = EditorSceneController()
@@ -46,12 +48,19 @@ class EditorScene(skin: Skin) : Scene, ChangeListener() {
                         addListener(this@EditorScene)
                     }).padLeft(10f).padRight(10f)
                 }).fillX().align(Align.top).expand()
+                row()
+                add(Table().apply {
+                    right()
+                    add(fpsLabel).padRight(10f)
+                    add(fpsValue).padRight(10f)
+                }).fillX().align(Align.bottom).expand()
                 setFillParent(true)
             }
 
     override fun draw() {
         controller.draw()
         stage.act(Gdx.graphics.deltaTime)
+        fpsValue.setText("${Gdx.graphics.framesPerSecond}")
         stage.draw()
     }
 
