707d3806a19c3da19cfa39dcd4642aa044d10cda
Dmytro Pashko <dmytro.pashko@globallogic.com>
2020-07-13 14:46:41 +0300

    Updated Box3dScene.kt

--- core/src/com/dpashko/sandbox/scene/box3d/Box3dScene.kt
+++ core/src/com/dpashko/sandbox/scene/box3d/Box3dScene.kt
@@ -2,9 +2,9 @@ package com.dpashko.sandbox.scene.box3d
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20.*
-import com.badlogic.gdx.graphics.Pixmap
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.VertexAttribute
+import com.badlogic.gdx.graphics.glutils.IndexBufferObjectSubData
 import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import com.badlogic.gdx.graphics.glutils.VertexBufferObjectWithVAO
 import com.badlogic.gdx.math.Matrix4
@@ -18,7 +18,8 @@ import kotlin.random.Random
 class Box3dScene @Inject internal constructor() : Scene {
 
     private lateinit var shader: ShaderProgram
-    private val boxes = mutableListOf<Box>()
+    private lateinit var boxes: Map<Box, Vector3>
+
     private val viewMatrix = Matrix4()
             .setToLookAt(Vector3(0f, -8f, 1f), Vector3.Zero, Vector3.Y)
     private val projectionMatrix = Matrix4()
@@ -31,118 +32,111 @@ class Box3dScene @Inject internal constructor() : Scene {
                 throw IllegalStateException("Shader is not compiled: $log")
             }
         }
-        createBoxes()
+        boxes = mapOf(
+                Box(position = Vector3(2f, 1f, -2f), texture = Texture(FileProvider.box)) to getRandomDirection(),
+                Box(position = Vector3(-2f, 1f, -2f), texture = Texture(FileProvider.box)) to getRandomDirection(),
+                Box(position = Vector3(0f, -2f, -2f), texture = Texture(FileProvider.box)) to getRandomDirection(),
+                Box(position = Vector3.Zero, texture = Texture(FileProvider.box)) to Vector3.Zero,
+                Box(position = Vector3(-2f, -1f, 2f), texture = Texture(FileProvider.box)) to getRandomDirection(),
+                Box(position = Vector3(2f, -1f, 2f), texture = Texture(FileProvider.box)) to getRandomDirection(),
+                Box(position = Vector3(0f, 2f, 2f), texture = Texture(FileProvider.box)) to getRandomDirection()
+        )
     }
 
-    private fun createBoxes() {
-        boxes.apply {
-            add(Box(Vector3(2f, 1f, -2f), getRandomDirection()))
-            add(Box(Vector3(-2f, 1f, -2f), getRandomDirection()))
-            add(Box(Vector3(0f, -2f, -2f), getRandomDirection()))
-            add(Box())
-            add(Box(Vector3(-2f, -1f, 2f), getRandomDirection()))
-            add(Box(Vector3(2f, -1f, 2f), getRandomDirection()))
-            add(Box(Vector3(0f, 2f, 2f), getRandomDirection()))
-        }
-    }
 
     private fun getRandomDirection() = Vector3(Random.nextFloat(), Random.nextFloat(), Random.nextFloat())
 
     override fun draw() {
         Gdx.gl.glEnable(GL_DEPTH_TEST)
         Gdx.gl.glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-        updateRotations()
+        // Rotate camera around Z axis by 0.25 degree.
+        viewMatrix.rotate(Vector3.Z, 0.25f)
         shader.begin()
         combinedCameraMatrix.set(projectionMatrix).mul(viewMatrix)
-        for (box in boxes) {
+        for ((box, rotationAxis) in boxes) {
+            box.rotate(rotationAxis, 0.25f)
             box.draw(shader, combinedCameraMatrix)
         }
         shader.end()
     }
 
-    private fun updateRotations() {
-        viewMatrix.rotate(Vector3.Z, 0.25f)
-        for (box in boxes) {
-            box.rotate()
-        }
-    }
-
     override fun dispose() {
-        for (box in boxes) {
+        for (box in boxes.keys) {
             box.dispose()
         }
         shader.dispose()
     }
 
-    data class Box(val pos: Vector3 = Vector3.Zero,
-                   val rotationDirection: Vector3 = Vector3.Zero,
-                   val texture: Texture = Texture(Pixmap(FileProvider.box))) {
+    data class Box(val position: Vector3, val size: Float = 1f, val texture: Texture) {
         private val vertices = createVertexData()
-        private val viewMatrix = Matrix4().translate(pos)
-        private val rotationSpeed = .2f
+        private val indices = createIndices()
+        private val viewMatrix = Matrix4().translate(position)
         private var tmp = Matrix4()
 
-        fun rotate() {
-            viewMatrix.rotate(rotationDirection, rotationSpeed)
-        }
-
         fun draw(shader: ShaderProgram, combinedCameraMatrix: Matrix4) {
-            Gdx.gl.glBindTexture(GL_TEXTURE_2D, texture.textureObjectHandle)
             shader.setUniformMatrix("combined", tmp.set(combinedCameraMatrix).mul(viewMatrix))
+            Gdx.gl.glBindTexture(GL_TEXTURE_2D, texture.textureObjectHandle)
             vertices.bind(shader)
-            Gdx.gl.glDrawArrays(GL_TRIANGLES, 0, vertices.numVertices)
+            Gdx.gl.glDrawElements(GL_TRIANGLES, indices.numIndices, GL_UNSIGNED_SHORT, indices.buffer)
             vertices.unbind(shader)
         }
 
+        fun rotate(axis: Vector3, degree: Float) {
+            viewMatrix.rotate(axis, degree)
+        }
+
+        private fun createIndices() = IndexBufferObjectSubData(true, 36).apply {
+            val indices = shortArrayOf(
+                    0, 1, 2, 2, 3, 0,       // Bottom face
+                    4, 5, 6, 6, 7, 4,       // Top face
+                    8, 9, 10, 10, 11, 8,    // Left face
+                    12, 13, 14, 14, 15, 12, // Right face
+                    16, 17, 18, 18, 19, 16, // Front face
+                    20, 21, 22, 22, 23, 20  // Back Face
+            )
+            setIndices(indices, 0, indices.size)
+        }
+
         private fun createVertexData() =
-                VertexBufferObjectWithVAO(true, 36, VertexAttribute.Position(), VertexAttribute.TexCoords(0)).apply {
+                VertexBufferObjectWithVAO(true, 24, VertexAttribute.Position(), VertexAttribute.TexCoords(0)).apply {
                     val vertices = floatArrayOf(
+                            // Bottom face
                             -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
                             0.5f, -0.5f, -0.5f, 1.0f, 0.0f,
                             0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
-                            0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
                             -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
-                            -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
-
+                            // Top face
                             -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
                             0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
                             0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
-                            0.5f, 0.5f, 0.5f, 1.0f, 1.0f,
                             -0.5f, 0.5f, 0.5f, 0.0f, 1.0f,
-                            -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-
+                            // Left face
                             -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
                             -0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
                             -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-                            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
                             -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-                            -0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
+                            // Right face
                             0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
                             0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
                             0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-                            0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
                             0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-                            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-
+                            // Front face
                             -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
                             0.5f, -0.5f, -0.5f, 1.0f, 1.0f,
                             0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
-                            0.5f, -0.5f, 0.5f, 1.0f, 0.0f,
                             -0.5f, -0.5f, 0.5f, 0.0f, 0.0f,
-                            -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-
+                            // Back Face
                             -0.5f, 0.5f, -0.5f, 0.0f, 1.0f,
                             0.5f, 0.5f, -0.5f, 1.0f, 1.0f,
                             0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-                            0.5f, 0.5f, 0.5f, 1.0f, 0.0f,
-                            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f,
-                            -0.5f, 0.5f, -0.5f, 0.0f, 1.0f)
+                            -0.5f, 0.5f, 0.5f, 0.0f, 0.0f
+                    )
                     setVertices(vertices, 0, vertices.size)
                 }
 
         fun dispose() {
             vertices.dispose()
+            indices.dispose()
             texture.dispose()
         }
     }
